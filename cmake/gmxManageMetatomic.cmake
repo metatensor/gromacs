1ac6b17a40fdb751db0cb4c6ee85881af01055f3
Rohit Goswami <rog32@hi.is>
2025-09-15 12:29:00 +0200

    chore(metatomic): rename for the new ecosystem

--- cmake/gmxManageMetatomic.cmake
+++ cmake/gmxManageMetatomic.cmake
@@ -32,39 +32,49 @@
 # the research papers on the package. Check out https://www.gromacs.org.
 
 
-option(GMX_METATENSOR "Enable interface to metatensor atomistic models" OFF)
+option(GMX_METATOMIC "Enable interface to metatomic atomistic models" OFF)
 
 # if(TORCH_ALREADY_SEARCHED)
 #     set(FIND_TORCH_QUIETLY ON)
 # endif()
 
-if(GMX_METATENSOR)
+if(GMX_METATOMIC)
     # For the initial development, we just build the metatebsor library as part
     # of the gromacs build process. Later we'll add an option to use
     # pre-installed versions of the libraries
 
     include(FetchContent)
 
-    set(URL_BASE "https://github.com/metatensor/metatensor/releases/download")
+    set(URL_METATENSOR_BASE "https://github.com/metatensor/metatensor/releases/download")
+    set(URL_METATOMIC_BASE "https://github.com/metatensor/metatomic/releases/download")
 
-    set(METATENSOR_CORE_VERSION "0.1.10")
+    set(METATENSOR_CORE_VERSION "0.1.17")
     FetchContent_Declare(metatensor
-        URL ${URL_BASE}/metatensor-core-v${METATENSOR_CORE_VERSION}/metatensor-core-cxx-${METATENSOR_CORE_VERSION}.tar.gz
-        URL_HASH SHA1=e89495c7ce425831257f66f0084ffb28caefc516
+        URL ${URL_METATENSOR_BASE}/metatensor-core-v${METATENSOR_CORE_VERSION}/metatensor-core-cxx-${METATENSOR_CORE_VERSION}.tar.gz
+        URL_HASH SHA256=42119e11908239915ccc187d7ca65449b461f1d4b5af4d6df1fb613d687da76a
     )
 
     message(STATUS "Fetching metatensor v${METATENSOR_CORE_VERSION} from github")
     FetchContent_MakeAvailable(metatensor)
 
-
-    set(METATENSOR_TORCH_VERSION "0.5.5")
-    FetchContent_Declare(metatensor-torch
-        URL ${URL_BASE}/metatensor-torch-v${METATENSOR_TORCH_VERSION}/metatensor-torch-cxx-${METATENSOR_TORCH_VERSION}.tar.gz
-        URL_HASH SHA1=ad0c6ad5c8ea364b0b85fd96fd656822fd1b1443
+    set(METATOMIC_CORE_VERSION "0.1.0")
+    FetchContent_Declare(metatomic
+        URL ${URL_METATOMIC_BASE}/metatomic-core-v${METATOMIC_CORE_VERSION}/metatomic-core-cxx-${METATOMIC_CORE_VERSION}.tar.gz
+        URL_HASH SHA256=a5fc42a847ba8bb70e1ab9eb421784d1085b3472c7df1e2c1af2d490fd4ed3bd
     )
 
-    message(STATUS "Fetching metatensor-torch v${METATENSOR_TORCH_VERSION} from github")
-    FetchContent_MakeAvailable(metatensor-torch)
+    message(STATUS "Fetching metatomic v${METATOMIC_CORE_VERSION} from github")
+    FetchContent_MakeAvailable(metatomic)
 
-    list(APPEND GMX_COMMON_LIBRARIES metatensor_torch)
+
+    set(METATOMIC_TORCH_VERSION "0.1.4")
+    FetchContent_Declare(metatomic-torch
+        URL ${URL_METATOMIC_BASE}/metatomic-torch-v${METATOMIC_TORCH_VERSION}/metatomic-torch-cxx-${METATOMIC_TORCH_VERSION}.tar.gz
+        URL_HASH SHA256=385ec8b8515d674b6a9f093f724792b2469e7ea2365ca596f574b64e38494f94
+    )
+
+    message(STATUS "Fetching metatomic-torch v${METATOMIC_TORCH_VERSION} from github")
+    FetchContent_MakeAvailable(metatomic-torch)
+
+    list(APPEND GMX_COMMON_LIBRARIES metatomic_torch)
 endif()
