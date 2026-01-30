bf0d90bef7f14a7432c4ef575a228b297b437c8d
Rohit Goswami <rgoswami@ieee.org>
2026-01-30 07:36:01 +0100

    chore(nnpot): cleaner fix

--- cmake/gmxManageNNPot.cmake
+++ cmake/gmxManageNNPot.cmake
@@ -61,23 +61,18 @@ if(NOT GMX_NNPOT STREQUAL "OFF")
     endif()
 
     # When we require at least CMake 4.1, finding Torch with OPTIONAL might be a good approach
+    if (NOT GMX_USE_NVTX)
+      # This toggle exists anyway
+      # Prevents build failures
+      if(NOT TARGET CUDA::nvToolsExt)
+        add_library(CUDA::nvToolsExt INTERFACE IMPORTED)
+      endif()
+    endif()
     find_package(Torch 2.0.0 QUIET)
     set(TORCH_ALREADY_SEARCHED TRUE CACHE BOOL "True if a search for libtorch has already been done")
     mark_as_advanced(TORCH_ALREADY_SEARCHED)
 
     if(Torch_FOUND)
-        # This toggle exists anyway
-        # Filter out nvToolsExt from TORCH_LIBRARIES to prevent build failures
-        if (NOT GMX_USE_NVTX)
-        set(_filtered_torch_libs "")
-        # TORCH_LIBRARIES contain imported target "torch" that will set all flags and include paths etc
-        foreach(_lib IN LISTS TORCH_LIBRARIES)
-            if(NOT _lib MATCHES "nvToolsExt")
-                list(APPEND _filtered_torch_libs "${_lib}")
-            endif()
-        endforeach()
-        endif()
-        
         # TORCH_LIBRARIES contain imported target "torch" that will set all flags and include paths etc
         list(APPEND GMX_COMMON_LIBRARIES ${_filtered_torch_libs})
         if(NOT FIND_TORCH_QUIETLY)
