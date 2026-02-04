6ed12a742b13e003f54eac33d4c2af9b333d59d0
Rohit Goswami <rgoswami@ieee.org>
2026-02-04 17:30:50 +0100

    chore(mtagro): switch to fprintf
    since GMX_LOG is rank 0 only, and cerr is not allowed as per GROMACS
    style guides

--- src/gromacs/applied_forces/metatomic/metatomic_forceprovider.cpp
+++ src/gromacs/applied_forces/metatomic/metatomic_forceprovider.cpp
@@ -406,15 +406,13 @@ void MetatomicForceProvider::gatherAtomNumbersIndices(const MDModulesAtomsRedist
                 // Match current local atom to one of the requested Metatomic input atoms
                 if (options_.params_.mtaAtoms_->globalIndex()[j] == globalIdx)
                 {
-                    // [DEBUG] Print distribution info
-        GMX_LOG(logger_.info)
-                .asParagraph().appendTextFormatted(
-                            "Rank %d: Found ModelAtom %d (Global %d) at Local %d (%s)\n",
-                            mpiComm_.rank(),
-                            j,
-                            globalIdx,
-                            i,
-                            (i < numLocal) ? "HOME" : "HALO");
+                    std::fprintf(stderr,
+                                 "Rank %d: Found ModelAtom %d (Global %d) at Local %d (%s)\n",
+                                 mpiComm_.rank(),
+                                 j,
+                                 globalIdx,
+                                 i,
+                                 (i < numLocal) ? "HOME" : "HALO");
                     if (i < numLocal)
                     {
                         inputToLocalIndex_[j]  = i;
@@ -437,12 +435,8 @@ void MetatomicForceProvider::gatherAtomNumbersIndices(const MDModulesAtomsRedist
                 localCount++;
             }
         }
-        GMX_LOG(logger_.info)
-                .asParagraph()
-                .appendTextFormatted("Rank %d: Mapped %d / %d Metatomic atoms (Home+Halo).",
-                                     mpiComm_.rank(),
-                                     localCount,
-                                     numInput);
+        fprintf(stderr, "Rank %d: Mapped %d / %d Metatomic atoms (Home+Halo).\n",
+                mpiComm_.rank(), localCount, numInput);
     }
     else
     {
